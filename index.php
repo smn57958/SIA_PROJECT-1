97fae52fc61626e78ae5b7a315d61b085fe3320c
Mr. Stark <starsohan@protonmail.com>
2017-08-24 13:21:46 -0400

    INIT

---
+++ index.php
@@ -0,0 +1,37 @@
+#-------------------------------------------------------------------------------
+# Copyright (C) 2017
+# Authors:
+# Sohan Nipunage <smn57958@uga.edu>
+# Dnyanada Shirsat <dds69748@uga.edu>
+# Akshay Agashe <aa84678@uga.edu>
+# Niyati Shah <nhs01063@uga.edu>
+# 
+# *For using this project anywhere, contact authors for permission*
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#-------------------------------------------------------------------------------
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Frameset//EN">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>AIS_project-1</title>
+</head>
+    <body>
+    <p>This page uses frames. The current browser you are using does not support frames.</p>
+    <?php
+$x=7;
+$y=15;
+echo "Sum is :".($x+$y);
+	?>
+    </body>
+</html>
